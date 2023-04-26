18f8f6b48f7b03424ea6e9ff59dd54bbb700282d
Nkunze Caleb <nkunzecaleb@gmail.com>
2023-04-26 02:56:32 -0700

    Update printf

--- _printf.c
+++ _printf.c
@@ -1,43 +1,5 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdarg.h>
-#include <string.h>
-<<<<<<< HEAD
-=======
-
-/**
- * print_char - prints a single character to stdout
- * @args: va_list containing the character to print
- * @count: pointer to the character count
- */
-void print_char(va_list args, int *count)
-{
-char c = (char) va_arg(args, int);
-putchar(c);
-(*count)++;
-}
-
-/**
- * print_string - prints a string to stdout
- * @args: va_list containing the string to print
- * @count: pointer to the character count
- */
-void print_string(va_list args, int *count)
-{
-char *str = va_arg(args, char *);
-fputs(str, stdout);
-(*count) += strlen(str);
-}
-
-/**
- * print_percent - prints a percent sign to stdout
- * @count: pointer to the character count
- */
-void print_percent(int *count)
-{
-putchar('%');
-(*count)++;
-}
->>>>>>> e959d3f920e44a970c1710022a46b676092732a5
 
 /**
  * _printf - prints formatted output to stdout
@@ -46,49 +8,41 @@ putchar('%');
  */
 int _printf(const char *format, ...)
 {
-int count = 0;
 va_list args;
+int count = 0;
+
 va_start(args, format);
 
-while (*format) {
-if (*format == '%') {
+while (*format != '\0')
+{
+if (*format == '%')
+{
 format++;
-<<<<<<< HEAD
-switch (*format) {
-case 'c':
-putchar(va_arg(args, int));
+if (*format == 'c')
+{
+char c = (char) va_arg(args, int);
+putchar(c);
 count++;
-break;
-case 's':
-fputs(va_arg(args, char*), stdout);
-count += strlen(va_arg(args, char*));
-break;
-case '%':
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+fputs(str, stdout);
+}
+else if (*format == '%')
+{
 putchar('%');
 count++;
-break;
-default:
-fprintf(stderr, "Invalid conversion specifier: %%%c\n", *format);
-return (-1);
 }
-} else {
-=======
-if (*format == 'c')
-print_char(args, &count);
-else if (*format == 's')
-print_string(args, &count);
-else if (*format == '%')
-print_percent(&count);
 else
 {
-print_percent(&count);
+putchar('%');
 putchar(*format);
-count++;
+count += 2;
 }
 }
 else
 {
->>>>>>> e959d3f920e44a970c1710022a46b676092732a5
 putchar(*format);
 count++;
 }
@@ -96,5 +50,6 @@ format++;
 }
 
 va_end(args);
+
 return (count);
 }
