70224d1613695b017969e551f605f9206efae9d3
sam <sookwalinga@gmail.com>
2023-04-26 12:48:40 +0300

    fix: more betty issues

--- _printf.c
+++ _printf.c
@@ -1,5 +1,40 @@
 #include "main.h"
 #include <stdarg.h>
+#include <string.h>
+
+/**
+ * print_char - prints a single character to stdout
+ * @args: va_list containing the character to print
+ * @count: pointer to the character count
+ */
+void print_char(va_list args, int *count)
+{
+char c = (char) va_arg(args, int);
+putchar(c);
+(*count)++;
+}
+
+/**
+ * print_string - prints a string to stdout
+ * @args: va_list containing the string to print
+ * @count: pointer to the character count
+ */
+void print_string(va_list args, int *count)
+{
+char *str = va_arg(args, char *);
+fputs(str, stdout);
+(*count) += strlen(str);
+}
+
+/**
+ * print_percent - prints a percent sign to stdout
+ * @count: pointer to the character count
+ */
+void print_percent(int *count)
+{
+putchar('%');
+(*count)++;
+}
 
 /**
  * _printf - prints formatted output to stdout
@@ -19,26 +54,16 @@ if (*format == '%')
 {
 format++;
 if (*format == 'c')
-{
-char c = (char) va_arg(args, int);
-putchar(c);
-count++;
-}
+print_char(args, &count);
 else if (*format == 's')
-{
-char *str = va_arg(args, char *);
-fputs(str, stdout);
-}
+print_string(args, &count);
 else if (*format == '%')
-{
-putchar('%');
-count++;
-}
+print_percent(&count);
 else
 {
-putchar('%');
+print_percent(&count);
 putchar(*format);
-count += 2;
+count++;
 }
 }
 else
