8bf00a53cb55c88c2e37512a5bd06c343c91738e
sam <sookwalinga@gmail.com>
2023-04-27 15:07:15 +0300

    refactor: betty was still sad

--- task5.c
+++ task5.c
@@ -6,7 +6,7 @@
  * print_hex - prints a hexadecimal number with at least 2 digits
  * @num: the number to print
  */
-static void print_hex(unsigned char num)
+void print_hex(unsigned char num)
 {
 if (num < 16)
 putchar('0');
@@ -14,26 +14,14 @@ printf("%X", num);
 }
 
 /**
- * print_string - prints a string with special formatting
+ * print_string - prints a string
  * @str: the string to print
- *
- * This function prints a string, but replaces non-printable characters
- * with the "\xXX" notation.
  */
-static void print_string(char *str)
+void print_string(char *str)
 {
 while (*str != '\0')
 {
-if (*str < ' ' || *str >= 127)
-{
-printf("\\x");
-print_hex(*str);
-}
-else
-{
-putchar(*str);
-}
-str++;
+putchar(*str++);
 }
 }
 
@@ -41,9 +29,45 @@ str++;
  * print_int - prints an integer
  * @num: the integer to print
  */
-static void print_int(int num)
+void print_int(int num)
 {
 printf("%d", num);
+}
+
+/**
+ * handle_conversion - handles a conversion character
+ * @p: a pointer to the conversion character
+ * @args: a va_list containing the arguments
+ *
+ * Return: the number of characters printed
+ */
+int handle_conversion(const char **p, va_list args)
+{
+int count = 0;
+
+/* handle %S */
+if (**p == 'S')
+{
+char *str = va_arg(args, char *);
+print_string(str);
+count += strlen(str);
+(*p)++;
+}
+/* handle %d */
+else if (**p == 'd')
+{
+int num = va_arg(args, int);
+print_int(num);
+count += 1;  /* count the number of digits printed */
+(*p)++;
+}
+/* handle other conversions */
+else
+{
+/* ... */
+}
+
+return (count);
 }
 
 /**
@@ -70,32 +94,7 @@ count++;
 continue;
 }
 
-/* handle %S */
-if (*(p + 1) == 'S')
-{
-char *str = va_arg(args, char *);
-print_string(str);
-count += strlen(str);
-p += 2;
-continue;
-}
-
-/* handle %d */
-if (*(p + 1) == 'd')
-{
-int num = va_arg(args, int);
-print_int(num);
-count += 1;  /* count the number of digits printed */
-p += 2;
-continue;
-}
-
-/* handle other conversions */
-/* ... */
-
-/* if we get here, we don't recognize the conversion */
-putchar(*p++);
-count++;
+count += handle_conversion(&p, args);
 }
 
 va_end(args);
