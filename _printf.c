2b9398d53f1cb59203d3a8c8e2255ce9cede7457
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-04-28 10:03:23 +0300

    edit printf

--- _printf.c
+++ _printf.c
@@ -1,31 +1,43 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
-int _printf(const char *format, ...) {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
+/**
+ * _printf - Print formatted output to stdout
+ * @format: The format string
+ * Return: The number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+int count = 0;
+va_list args;
+va_start(args, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    count += putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    count += printf("%s", va_arg(args, char *));
-                    break;
-                case '%':
-                    count += putchar('%');
-                    break;
-            }
-        } else {
-            count += putchar(*format);
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+count += putchar(va_arg(args, int));
+}
+else if (*format == 's')
+{
+count += printf("%s", va_arg(args, char *));
+}
+else if (*format == '%')
+{
+count += putchar('%');
+}
+else
+{
+printf("Error: Invalid conversion specifier");
+return (-1);
+}
+}
+else
+count += putchar(*format);
+format++;
+}
+va_end(args);
+return (count);
 }
